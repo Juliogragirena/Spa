08f84e72ade3511ef0aec71ce5abc85f2d69179f
Julio Gragirena <juliogragirena@hotmail.com>
2020-07-08 23:15:19 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Header from './templates/Header'
+
+const hola = document.getElementsByTagName('body')[0]
+console.log(document.hola)
+console.log(hola)
+hola.innerHTML = Header()
+hola.style="background-color:red"
