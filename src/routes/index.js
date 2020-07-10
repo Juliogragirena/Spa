0e81af87987a2a3c1acadc2d6614f019845d117d
Julio Gragirena <juliogragirena@hotmail.com>
2020-07-10 08:15:13 -0400

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
 import getHash from '../utils/getHash';
-import resolveRoutes from '../utils/resolveRoutes'
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
     '/': Home,
     '/:id':Character,
-    '/contact': 'Contact'
+    '/contact': 'Contact',
 };
 
 const router = async () => {
@@ -16,9 +16,9 @@ const router = async () => {
     const content = null || document.getElementById('content');
     header.innerHTML = await Header();
     let hash = getHash();
-    let route = await resolveRoutes(hash)
+    let route = await resolveRoutes(hash);
     let render = routes[route] ? routes[route] : Error404;
-    content.innerHTML = await render()
+    content.innerHTML = await render();
 }
 
 
